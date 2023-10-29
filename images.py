0a1382967d0bb5dcdfc2640d2c05a3f73fdec188
@jfcorreas <jfcorreas@gmail.com>
2023-10-29 17:34:09 +0100

    Force result images to JPEG format

--- images.py
+++ images.py
@@ -6,4 +6,5 @@ dest_path = Path.cwd() / 'icons'
 
 for f in Path(images_path).iterdir():
   img = Image.open(f)
-  img.rotate(90).resize((128,128)).save(dest_path / f.with_suffix('.jpeg').name) 
+  # img.convert('RGB')  Necessary if source format is TIFF
+  img.rotate(90).resize((128,128)).save(dest_path / f.with_suffix('.jpeg').name, "JPEG") 
