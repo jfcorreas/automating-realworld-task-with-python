ac2ace86edce6a888420eb94097b8b790dfd54ea
@jfcorreas <jfcorreas@gmail.com>
2023-11-05 13:24:52 +0100

    Analyze car sales and shows a sales summary

--- scripts/cars.py
+++ scripts/cars.py
@@ -18,5 +18,11 @@ with open("data/car_sales.json", "r") as file:
     else:
       most_sales_by_year[car_year] = car_sales
 
-print(dict(sorted(most_sales_by_model.items(), key=lambda item: item[1], reverse=True)))
-print(dict(sorted(most_sales_by_year.items(), key=lambda item: item[1], reverse=True)))
+most_sales_by_model_sorted = dict(sorted(most_sales_by_model.items(), key=lambda item: item[1], reverse=True))
+most_sales_by_year_sorted = dict(sorted(most_sales_by_year.items(), key=lambda item: item[1], reverse=True))
+
+most_sales_model = list(most_sales_by_model_sorted.keys())[0]
+print('The {} had the most sales: {}'.format(most_sales_model, most_sales_by_model[most_sales_model]))
+
+most_sales_year = list(most_sales_by_year_sorted.keys())[0]
+print('The most popular year was {} with {} sales.'.format(most_sales_year, most_sales_by_year[most_sales_year]))
