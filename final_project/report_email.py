1043f643f98e4aed5f98126ca00e1afcb7cdc332
@jfcorreas <jfcorreas@gmail.com>
2023-11-19 20:31:46 +0100

    FP: add emails.py module and use it from report_email.py

--- final_project/report_email.py
+++ final_project/report_email.py
@@ -3,6 +3,7 @@
 from pathlib import Path
 from datetime import datetime
 import reports
+import emails
 
 source_path = Path.cwd() / 'supplier-data' / 'descriptions'
 report_path = '/tmp/processed.pdf'
@@ -18,6 +19,15 @@ def main():
       report_paragraph.append('name: {}'.format(description_line[0].rstrip()))
       report_paragraph.append('weight: {}'.format(description_line[1].rstrip()))
   reports.generate_report(report_path, report_title, '<br/>'.join(report_paragraph))
+
+# Send the PDF report as an email attachment
+
+#sender = "automation@example.com"
+#receiver = "{}@example.com".format(os.environ.get('USER'))
+#subject = "Upload Completed - Online Fruit Store"
+#body = "All fruits are uploaded to our website successfully. A detailed list is attached to this email."
+#message = emails.generate_email(sender, receiver, subject, body, report_path)
+#emails.send_email(message)
 
 if __name__ == "__main__":
     main()
