3981ce05182e7cb2c5f23a5d6f7982177d83e802
@jfcorreas <jfcorreas@gmail.com>
2023-11-21 23:43:13 +0100

    FP: supplier_image_upload final version

--- final_project/supplier_image_upload.py
+++ final_project/supplier_image_upload.py
@@ -8,6 +8,5 @@ url = "http://localhost/upload/"
 
 for image_file in Path(images_path).iterdir():
   if image_file.suffix == '.jpeg':
-    print(image_file)
     with open(image_file, 'rb') as opened:
       r = requests.post(url, files={'file': opened})
