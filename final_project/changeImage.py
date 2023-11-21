44d1e2902204a49e1e974ebe81278c86c05f542e
@jfcorreas <jfcorreas@gmail.com>
2023-11-21 23:37:38 +0100

    FP: change Image final version

--- final_project/changeImage.py
+++ final_project/changeImage.py
@@ -6,6 +6,6 @@ from pathlib import Path
 images_path = Path.cwd() / 'supplier-data' / 'images'
 
 for f in Path(images_path).iterdir():
-  img = Image.open(f)
-  img.convert('RGB')  # Necessary if source format is RGBA
-  img.resize((600,400)).save(images_path / f.with_suffix('.jpeg').name, "JPEG") 
+  if f.suffix == '.tiff':
+    img = Image.open(f)
+    img.convert('RGB').resize((600,400)).save(images_path / f.with_suffix('.jpeg').name, "JPEG") 
